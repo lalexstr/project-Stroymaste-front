77466021f674d5303174b89a5426f2f72342107d
lalexstr <alexstroilov2007@icloud.com>
2025-05-19 19:43:48 +1000

    server 5000

--- vite.config.js
+++ vite.config.js
@@ -15,8 +15,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  server: {
+    port: 5000
+  },
   assetsInclude: ['**/*.jpg', '**/*.png', '**/*.gif'],
 })
-
 
 
